d05b708bb6db605ddd8c2efcb8bbe5d31890ed64
sncj <selemerico@yahoo.com.br>
2022-01-22 18:23:31 -0300

    segundo commit!

---
+++ contas/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Categoria
+from .models import Transacao
+
+admin.site.register(Categoria)
+admin.site.register(Transacao)
